783ac10aa01ddcfa9dc562ce0f23e511f9be1ed8
Eric Wollesen <ericw@xmtp.net>
2011-06-23 14:15:42 -0600

    The time response from the status command is a pair of times, not a single time.

--- lib/rmpd/response.rb
+++ lib/rmpd/response.rb
@@ -3,8 +3,9 @@ require "delegate"
 module Rmpd
 
   KEY_VALUE_RE = /^([^:]+):\s*(.*)$/
-  KNOWN_INT_FIELDS = [:time, :pos, :id, :track, :playlistlength, :playlist,
+  KNOWN_INT_FIELDS = [:pos, :id, :track, :playlistlength, :playlist,
     :xfade, :repeat, :random, :queued, :volume, :song]
+  KNOWN_COMPLEX_FIELDS = [:time,]
 
   class Response < DelegateClass(Hash)
 
@@ -29,6 +30,7 @@ module Rmpd
     def register_key_val_pair(r)
       key, val = r[1].downcase.to_sym, r[2]
       val = val.to_i if KNOWN_INT_FIELDS.include?(key)
+      val = send("parse_complex_#{key}", val) if KNOWN_COMPLEX_FIELDS.include?(key)
       self[key] = include?(key) ? ([self[key]] << val).flatten : val
       self.class.send(:define_method, key) {self[key]}
     end
@@ -40,6 +42,15 @@ module Rmpd
         when OK_RE: @error = nil
         when ACK_RE: @error = MpdAckError.new($~.values_at(0..-1))
         end
+      end
+    end
+
+    # time can be either an integer (playlistinfo) or elapsed:total (status)
+    def parse_complex_time(value)
+      if /:/ === value
+        value.split(":", 2).map(&:to_i)
+      else
+        value.to_i
       end
     end
 
