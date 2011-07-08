cbbe038a29701ac26a89b927adc40db788f0c759
Eric Wollesen <ericw@xmtp.net>
2011-07-07 21:17:38 -0600

    Fix addid once again.
    I need to trust my tests more.

--- lib/rmpd/commands/playlist.rb
+++ lib/rmpd/commands/playlist.rb
@@ -36,7 +36,9 @@ module Rmpd
     def addid(path, pos=nil)
       # pos is only for r7153+, but what version is that?
       server_version_at_least(0, 14, 0) if pos
-      send_command("addid #{quote([path])} #{pos ? pos.to_s : nil}")
+      args = [path]
+      args << pos if pos
+      send_command("addid", *quote(args))
       @add_id_response_regex ||= /(^Id: )/i
       if @in_command_list
         append_command_list_regexp(@add_id_response_regex)
--- lib/rmpd/version.rb
+++ lib/rmpd/version.rb
@@ -1,3 +1,3 @@
 module Rmpd
-  VERSION = "1.0.3"
+  VERSION = "1.0.4"
 end
