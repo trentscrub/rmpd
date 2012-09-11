6d3674136a59a7fdd9197de6a764b4425cc8a282
Eric Wollesen <eric@leadtune.com>
2012-09-11 10:37:07 -0600

    Improve noidle support
    Don't call noidle unless we're in an idle loop.

--- lib/rmpd/commands/miscellaneous.rb
+++ lib/rmpd/commands/miscellaneous.rb
@@ -4,8 +4,8 @@ module Rmpd
     simple_command :commands
     simple_command :notcommands
     simple_command :clearerror
-    simple_command :idle
-    simple_command :noidle
+    simple_command :_idle
+    simple_command :_noidle
     simple_command :password
     simple_command :ping
     simple_command :stats
@@ -19,6 +19,18 @@ module Rmpd
 
     simple_command :command_list
     simple_command :command_list_ok
+
+    def idle(*subsystems)
+      @in_idle = true
+      _idle(mpd, *subsystems)
+    ensure
+      @in_idle = false
+    end
+
+    def noidle
+      return unless @in_idle
+      _noidle
+    end
 
     alias_method :clear_error, :clearerror
     alias_method :not_commands, :notcommands
--- lib/rmpd/version.rb
+++ lib/rmpd/version.rb
@@ -1,3 +1,3 @@
 module Rmpd
-  VERSION = "1.1.0"
+  VERSION = "1.1.1"
 end
